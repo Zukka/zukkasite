3a08b8773da210422b96a3786173c1990a54aa01
Alessandro <a.bellotti@patente.it>
2025-01-28 17:37:14 +0100

    Added Google Adsense

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import GoogleAdsense from "./components/GoogleAdsense";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -14,9 +15,12 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+const apiKey = process.env.REACT_APP_ADSENSE_KEY
+
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <GoogleAdsense pId={apiKey ?? ""} />
     </html>
   );
 }
