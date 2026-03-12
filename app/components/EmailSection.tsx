4c20905825d216b5f6166ced7a99a5e57f88d2a4
Alessandro Bellotti <alessandro.bellotti@cefriel.com>
2026-03-12 16:50:47 +0100

    add logs

--- app/components/EmailSection.tsx
+++ app/components/EmailSection.tsx
@@ -36,6 +36,10 @@ const EmailSection: React.FC<EmailSectionProps> = ({
       console.log('templateId:', templateId);
       console.log('userId:', userId);
 
+      console.log('serviceId:', serviceId);
+      console.log('templateId:', templateId);
+      console.log('userId:', userId);
+
       const response = await emailjs.send(
         serviceId,
         templateId,
